1e327a46943985be2270e5e5a5953cf91e8bca75
Akshayy <mote.akshay.18et1011@gmail.com>
2024-07-27 23:38:38 +0530

    updates..

--- server-side/index.js
+++ server-side/index.js
@@ -1,8 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Home Page')
+});
 
-app.listen(4000);
+dotenv.config();
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("Database connected");
+});
+
+app.listen(process.env.PORT_NUMBER, () => {
+  console.log(`server is running on port ${process.env.PORT_NUMBER}`);
+});
